8ac4397b159ab9404b0f934aba328217e4788fe5
yeonwooPark <yeonwoo5115@gmail.com>
2024-04-05 11:45:25 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.sparta.springauth.controller;
 
-import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
@@ -30,11 +34,18 @@ public class UserController {
   }
 
   @PostMapping("/user/signup")
-  public String signup(SignupRequestDto requestDto) {
+  public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    // Validation 예외처리
+    List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+    if(fieldErrors.size() > 0) {
+      for (FieldError fieldError : bindingResult.getFieldErrors()) {
+        log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+      }
+      return "redirect:/api/user/signup";
+    }
+
     userService.signup(requestDto);
+
     return "redirect:/api/user/login-page";
   }
-
-
-
 }
