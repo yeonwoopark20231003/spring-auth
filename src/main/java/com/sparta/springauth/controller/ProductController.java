fa4677b039ae71d13f8da78b681e7189cbf5b77f
yeonwooPark <yeonwoo5115@gmail.com>
2024-04-05 10:35:59 +0900

    접근불가 페이지

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,4 +23,16 @@ public class ProductController {
 
     return "redirect:/";
   }
+
+  @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+  @GetMapping("/products/secured")
+  public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+    for (GrantedAuthority authority : userDetails.getAuthorities()) {
+      System.out.println("authority.getAuthority() = " + authority.getAuthority());
+    }
+
+    return "redirect:/";
+  }
+
 }
--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -35,16 +35,6 @@ public class UserController {
     return "redirect:/api/user/login-page";
   }
 
-  @PostMapping("/user/login")
-  public String login(LoginRequestDto requestDto, HttpServletResponse res){
-    try {
-      userService.login(requestDto, res);
-    } catch (Exception e) {
-      return "redirect:/api/user/login-page?error";
-    }
-
-    return "redirect:/";
-  }
 
 
 }
